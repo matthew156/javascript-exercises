a31482d99d63ab2b94ded32bfc3207edd77ceccf
Matt <matthewastro@gmail.com>
2022-03-18 23:54:03 -0400

    Passed all of the tests for Part 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,25 @@
-const ftoc = function() {
+const ftoc = function(farenheit) {
+let conversion = (farenheit-32)*(5/9)
+if(conversion%1 == 0)
+return Number(conversion);
 
+else{
+console.log(typeof conversion.toFixed(1))
+  return Number(conversion.toFixed(1))
+
+}
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+let conversion = (celsius*(9/5))+32
+if(conversion%1 == 0)
+return Number(conversion);
 
+else
+return Number(conversion.toFixed(1))
 };
+
+
 
 // Do not edit below this line
 module.exports = {
