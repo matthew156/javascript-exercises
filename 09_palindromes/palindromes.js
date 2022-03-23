6019a530bb0a488447ea3acc314e9f8efdd27b93
Matt <matthewastro@gmail.com>
2022-03-23 16:50:10 -0400

    completed the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+let reversedString = string.toLowerCase().split("")
+for(let i = 0; i < reversedString.length; i++)
+{
+    if(reversedString[i] ==="!"||reversedString[i] ==="." || reversedString[i] ==="," || reversedString[i] ===`'`|| reversedString[i] ==="?"||reversedString[i] ===`"` || reversedString[i] ===" ")
+    {
+        reversedString.splice(i, 1);
+        i--;
+    }
+}
+let savedString = reversedString.join("")
+return reversedString.reverse().join("") == savedString;
 };
-
+console.log(palindromes("racecar!"))
 // Do not edit below this line
 module.exports = palindromes;
