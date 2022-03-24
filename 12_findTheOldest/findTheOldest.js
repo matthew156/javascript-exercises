f08b5c7651ce23a37fc8c0292d635d67f542baab
Matt <matthewastro@gmail.com>
2022-03-23 21:47:13 -0400

    Review videos for the odin project

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+return arr.reduce((a,b) =>{
+    const oldestAge = getAge(a.yearOfBirth, a.yearOfDeath)
+    const currentAge = getAge(b.yearOfBirth, b.yearOfDeath)
+    return oldestAge < currentAge ? b : a
+})
+
+
+function getAge(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
 
 };
 
